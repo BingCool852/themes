66fd062d42364b8c0e93c6f028e28e3b97815e25
唐良成 <727288093@qq.com>
2015-12-29 20:53:51 +0800

    搜索2

--- cosimo/search.php
+++ cosimo/search.php
@@ -13,7 +13,7 @@ get_header(); ?>
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( '搜索结果来自: %s', 'cosimo' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+				<h1 class="page-title"><?php printf( esc_html__( '搜索结果: %s', 'cosimo' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ global $i; $i = 0; ?>
