6820a6db579efbe6aca71cf2f8f2a7a73c7c1d9c
唐良成 <727288093@qq.com>
2015-12-29 14:52:14 +0800

    评论 thoughts

--- cosimo/comments.php
+++ cosimo/comments.php
@@ -26,7 +26,7 @@ if ( post_password_required() ) {
 		<h2 class="comments-title">
 			<?php
 				printf(
-					esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'cosimo' ) ),
+					esc_html( _nx( '1条想法 &ldquo;%2$s&rdquo;', '%1$s 条想法 &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'cosimo' ) ),
 					number_format_i18n( get_comments_number() ),
 					'<span>' . get_the_title() . '</span>'
 				);
