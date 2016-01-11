988084e5afd5f2b520ce62710f2d4fe26062b4d1
唐良成 <727288093@qq.com>
2016-01-11 13:43:20 +0800

    footer修改

--- cosimo/footer.php
+++ cosimo/footer.php
@@ -23,39 +23,13 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info smallPart">
 			<div class="infoFoo">
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'cosimo' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'cosimo' ), 'WordPress' ); ?></a>
+				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'cosimo' ) ); ?>"><?php printf( esc_html__( '自豪的使用了 %s', 'cosimo' ), 'WordPress' ); ?></a>
 				<span class="sep"> | </span>
-				<?php printf( esc_html__( '%1$s by %2$s.', 'cosimo' ), 'Cosimo Free', '<a target="_blank" href="http://crestaproject.com/" rel="designer" title="CrestaProject">CrestaProject</a>' ); ?>
+				<?php printf( esc_html__( '%1$s by %2$s.', 'cosimo' ), 'cosimo主题', '<a target="_blank" href="http://crestaproject.com/" rel="designer" title="CrestaProject">CrestaProject</a>' ); ?>
 			</div>
 			<div class="infoFoo right">
 				<div class="socialLine">
-					<?php if (!empty($facebookURL)) : ?>
-						<a href="<?php echo esc_url($facebookURL); ?>" title="<?php esc_attr_e( 'Facebook', 'cosimo' ); ?>"><i class="fa fa-facebook spaceLeftRight"><span class="screen-reader-text"><?php esc_attr_e( 'Facebook', 'cosimo' ); ?></span></i></a>
-					<?php endif; ?>
-					<?php if (!empty($twitterURL)) : ?>
-						<a href="<?php echo esc_url($twitterURL); ?>" title="<?php esc_attr_e( 'Twitter', 'cosimo' ); ?>"><i class="fa fa-twitter spaceLeftRight"><span class="screen-reader-text"><?php esc_attr_e( 'Twitter', 'cosimo' ); ?></span></i></a>
-					<?php endif; ?>
-					<?php if (!empty($googleplusURL)) : ?>
-						<a href="<?php echo esc_url($googleplusURL); ?>" title="<?php esc_attr_e( 'Google Plus', 'cosimo' ); ?>"><i class="fa fa-google-plus spaceLeftRight"><span class="screen-reader-text"><?php esc_attr_e( 'Google Plus', 'cosimo' ); ?></span></i></a>
-					<?php endif; ?>
-					<?php if (!empty($linkedinURL)) : ?>
-						<a href="<?php echo esc_url($linkedinURL); ?>" title="<?php esc_attr_e( 'Linkedin', 'cosimo' ); ?>"><i class="fa fa-linkedin spaceLeftRight"><span class="screen-reader-text"><?php esc_attr_e( 'Linkedin', 'cosimo' ); ?></span></i></a>
-					<?php endif; ?>
-					<?php if (!empty($instagramURL)) : ?>
-						<a href="<?php echo esc_url($instagramURL); ?>" title="<?php esc_attr_e( 'Instagram', 'cosimo' ); ?>"><i class="fa fa-instagram spaceLeftRight"><span class="screen-reader-text"><?php esc_attr_e( 'Instagram', 'cosimo' ); ?></span></i></a>
-					<?php endif; ?>
-					<?php if (!empty($youtubeURL)) : ?>
-						<a href="<?php echo esc_url($youtubeURL); ?>" title="<?php esc_attr_e( 'YouTube', 'cosimo' ); ?>"><i class="fa fa-youtube spaceLeftRight"><span class="screen-reader-text"><?php esc_attr_e( 'YouTube', 'cosimo' ); ?></span></i></a>
-					<?php endif; ?>
-					<?php if (!empty($pinterestURL)) : ?>
-						<a href="<?php echo esc_url($pinterestURL); ?>" title="<?php esc_attr_e( 'Pinterest', 'cosimo' ); ?>"><i class="fa fa-pinterest spaceLeftRight"><span class="screen-reader-text"><?php esc_attr_e( 'Pinterest', 'cosimo' ); ?></span></i></a>
-					<?php endif; ?>
-					<?php if (!empty($tumblrURL)) : ?>
-						<a href="<?php echo esc_url($tumblrURL); ?>" title="<?php esc_attr_e( 'Tumblr', 'cosimo' ); ?>"><i class="fa fa-tumblr spaceLeftRight"><span class="screen-reader-text"><?php esc_attr_e( 'Tumblr', 'cosimo' ); ?></span></i></a>
-					<?php endif; ?>
-					<?php if (!empty($vkURL)) : ?>
-						<a href="<?php echo esc_url($vkURL); ?>" title="<?php esc_attr_e( 'VK', 'cosimo' ); ?>"><i class="fa fa-vk spaceLeftRight"><span class="screen-reader-text"><?php esc_attr_e( 'VK', 'cosimo' ); ?></span></i></a>
-					<?php endif; ?>
+					
 				</div>
 			</div>
 		</div><!-- .site-info -->
